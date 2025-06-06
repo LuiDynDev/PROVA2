37eb6c092bb16b1865eff1db00472bb286018b91
LUIS GABRIEL POLLIZELI ROSA <gabrielpolizelli908@gmail.com>
2025-06-06 17:09:19 -0300

    Comentarios cliente

--- Cadastro de Clientes.cs
+++ Cadastro de Clientes.cs
@@ -1,14 +1,19 @@
 using System;
 
 namespace LojaVirtual
-{
+{  
+    //cliente da loja virtual
     public class Cliente
     {
+        //Cliente unico
         public Guid Id { get; }
+        //Nome do cliente
         public string Nome { get; }
+        //Email do usuario
         public string Email { get; }
+        //CPF usuario
         public string Cpf { get; }
-
+        //Construtor e verificador de Nome, Email e CPF
         public Cliente(string nome, string email, string cpf)
         {
             if (string.IsNullOrWhiteSpace(nome))
@@ -19,7 +24,8 @@ namespace LojaVirtual
 
             if (string.IsNullOrWhiteSpace(cpf))
                 throw new ArgumentException("CPF é obrigatório.");
-
+                
+            // Atribui os valores e gera um ID
             Id = Guid.NewGuid();
             Nome = nome;
             Email = email;
