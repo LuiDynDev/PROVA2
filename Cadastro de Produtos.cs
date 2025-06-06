c6dc7332ca50c6cfbc9d4c096577b0f37d6f232e
LUIS GABRIEL POLLIZELI ROSA <gabrielpolizelli908@gmail.com>
2025-06-06 13:43:08 -0300

    Comentarios

--- Cadastro de Produtos.cs
+++ Cadastro de Produtos.cs
@@ -4,11 +4,15 @@ namespace LojaVirtual
 {
     public class Produto
     {
+        //ID do produto
         public Guid Id { get; }
+        //Nome do produto
         public string Nome { get; }
+        //Valor do produto
         public decimal Preco { get; }
+        //Categoria do produto
         public string Categoria { get; }
-
+        //Construtor da classe e validação Nome, Preço e Categoria do produto
         public Produto(string nome, decimal preco, string categoria)
         {
             if (string.IsNullOrWhiteSpace(nome))
@@ -19,7 +23,7 @@ namespace LojaVirtual
 
             if (string.IsNullOrWhiteSpace(categoria))
                 throw new ArgumentException("Categoria é obrigatória.");
-
+            // Adiciona valores e gera um ID único 
             Id = Guid.NewGuid();
             Nome = nome;
             Preco = preco;
